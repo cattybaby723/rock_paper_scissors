ef6fd54ff1010f1fce3666cea6c8f1c1ca85759a
anrou_hu <anrou_hu@hiiir.com>
2017-08-04 19:05:29 +0800

    @update the basic app structure

---
+++ app/src/main/java/com/example/anrou_hu/rock_paper_scissors/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.anrou_hu.rock_paper_scissors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
